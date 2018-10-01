0828628c6403b888734a238e8dbad921d330941d
Lowell Abbott <lowell@instacarro.com>
2018-10-01 00:15:11 -0400

    Add parametriced options for creating custom subnets in x region. Add 2 ELB with propoer scaling groups and lots of params. Add more extensive documentation

--- vpc_setup.tf
+++ vpc_setup.tf
@@ -37,7 +37,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "${var.aws_region}a"
 
     tags {
         Name = "main-public-1"
@@ -47,7 +47,7 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "${var.aws_region}b"
 
     tags {
         Name = "main-public-2"
@@ -57,7 +57,7 @@ resource "aws_subnet" "main-public-3" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
+    availability_zone = "${var.aws_region}c"
 
     tags {
         Name = "main-public-3"
@@ -67,7 +67,7 @@ resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "${var.aws_region}a"
 
     tags {
         Name = "main-private-1"
@@ -77,7 +77,7 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "${var.aws_region}b"
 
     tags {
         Name = "main-private-2"
@@ -87,7 +87,7 @@ resource "aws_subnet" "main-private-3" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.6.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
+    availability_zone = "${var.aws_region}c"
 
     tags {
         Name = "main-private-3"
